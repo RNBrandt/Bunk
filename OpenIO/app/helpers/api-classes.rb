abaff0a0b674316c7f5d8f9f51913b817ea3e3a8
Reuben Brandt <Reubennbrandt@gmail.com>
2016-06-05 11:45:01 -0700

    add translated initialize method to API class

---
+++ OpenIO/app/helpers/api-classes.rb
@@ -0,0 +1,13 @@
+class Api(object)
+# The base class for all API
+
+  def initialize(api_object, endpoint_url)
+    @version = "v0.1"
+    # @service = nil
+    parsed = URI.parse(endpoint_url)
+    @proxyd_host = parsed.host
+    @endpoint_uri = "#{parsed.path}/@version"
+  end
+
+
+end
